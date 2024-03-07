1cccc705f871456fa61adabb81a2ec16c537ff58
wharton-git <whartonaldrick@gmail.com>
2024-03-07 04:26:38 +0300

    Merge WIth Laravel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -6,27 +6,23 @@ import Club from './components/club.jsx';
 import Sport from './components/sports.jsx';
 
 import './App.css';
-import {Routes , Route , Link} from "react-router-dom";
-import Home from './pages/home'
+import Connex from './pages/home'
 import Create from './pages/create'
-import Chat from './pages/Chat'
 
 function App() {
 
 
   return (
-    <Router> {/* Enveloppez votre application dans le composant Router */}
-      <div>
-      <Routes>
-          <Route path="/" element={<Index />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/sport" element={<Sport />} />
-          <Route path="/club" element={<Club />} />
-        </Routes>
-        {/* Définissez vos Routes ici */}
+    <div>
 
-      </div>
-    </Router>
+        <Routes>
+            <Route path="/" element={<Index />} />
+            <Route path="/home" element={<Home />} />
+            <Route path="/sport" element={<Sport />} />
+            <Route path="/club" element={<Club />} />
+          </Routes>
+
+    </div>
   );
 }
 
